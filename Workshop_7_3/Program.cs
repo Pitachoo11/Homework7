a1c6eefafcd038ac7d5c5706e0e3d118001ce04d
Petr.Churkin <Petr.Churkin@gmail.com>
2022-11-30 10:39:15 +0300

    Workshop_7_3

---
+++ Workshop_7_3/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+
+int[,] GetArray(int m, int n, int minValue, int maxValue) 
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    PrintArray(result);
+    Console.WriteLine();
+    
+    EvenIndexPow2(result);
+    return result;
+}
+
+void EvenIndexPow2(int[,] inArray)
+{
+    for (int k = 0; k < inArray.GetLength(0); k+=2)
+    {
+        for (int l = 0; l < inArray.GetLength(1); l+=2)
+        {
+            inArray[k, l] *= inArray[k, l];
+        }
+    }
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+
+Console.Write("Введите кол-во строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите кол-во столбцов массива: ");
+int cols = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(rows, cols, 0, 10);
+PrintArray(array);
